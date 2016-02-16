a7c9f77152ffaf74ca4fedc1dc31ec239cf3eb29
JRavet <ravetjs@plu.edu>
2016-02-15 18:03:16 -0800

    Core code established; the program obtains match-details from the API, processes the match_details, actiivity_data and guild information from the retrieved data; basic functioning is nearly established

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//VERSION 1.0.0.0
+//VERSION 1.0.0.1
 #include <iostream>
 //cURLpp
 #include <curlpp/cURLpp.hpp>
@@ -24,36 +24,194 @@ using namespace Options;
 using namespace std;
 /* */
 int microSec = 1000000; //used to convert from microseconds to seconds and visa versa
+bool stored_matchDetails = false;
 /* */
-void storeMapData(const Json::Value map)
+void convertNumToString(stringstream *converter, float valueToConvert, string *returnString)
 {
-	const Json::Value objectives = map["objectives"];
-	cout << objectives[0]["claimed_at"];
-	cout << objectives[0]["claimed_by"];
-	cout << objectives[0]["id"];
-	cout << objectives[0]["last_flipped"];
-	cout << objectives[0]["owner"];
-	cout << objectives[0]["type"];
+	(*converter) << valueToConvert;
+	(*returnString) += "," + converter->str();
+	converter->str("");
+	converter->clear();
 }
 /* */
+void check_guildClaim(string guildID, sql::Connection *con)
+{
+	sql::Statement *stmt;
+	sql::ResultSet *res;
+	stmt = con->createStatement();
+	try
+	{
+		res = stmt->executeQuery("select * from guild where guild_id=\"" + guildID +"\"");
+	}
+	catch (sql::SQLException &e)
+	{
+		cout << e.what() << endl;
+		return;
+	}
+	if (!res->next()) //if there were no matching guild ids ...
+	{
+		Easy request;
+		stringstream guildDetails;
+		Json::Value root;
+		Json::Reader reader;
+		/* */
+		request.setOpt(cURLpp::Options::WriteStream(&guildDetails));
+		request.setOpt(Url("https://api.guildwars2.com/v1/guild_details.json?guild_id=" + guildID));
+		request.perform();
+		/* */
+		bool parsingSuccessful = reader.parse(guildDetails.str(), root);
+		if (!parsingSuccessful)
+		{
+			cout << "Failed to parse configuration\n" 
+				<< reader.getFormattedErrorMessages();
+			exit(0);
+		}
+		/* */
+		try
+		{
+			stmt->execute("INSERT INTO guild VALUES(\"" + guildID + "\",\"" + root["guild_name"].asString() + "\",\"" + root["tag"].asString() + "\");");
+		}
+		catch (sql::SQLException &e)
+		{
+			cout << e.what() << endl;
+		}
+	}
+	delete stmt;
+	delete res;
+}
+/* */
+void store_MapData(const Json::Value *root, int mapNum, sql::Connection *con)
+{
+	stringstream converter;
+	sql::Statement *stmt;
+	string SQLstmt = "";
+	const Json::Value objectives = (*root)["maps"][mapNum]["objectives"];
+	for (int i = 0; i < (int)objectives.size(); i++)
+	{
+		SQLstmt = "INSERT INTO activity_data VALUES(";
+		SQLstmt += "\"" + objectives[i]["last_flipped"].asString() + "\"";
+		SQLstmt += ",\"" + objectives[i]["id"].asString() + "\"";
+		//
+		if (objectives[i]["owner"].asString() == "Red")
+		{
+			convertNumToString(&converter,(*root)["worlds"]["red"].asInt(),&SQLstmt);
+		}
+		else if (objectives[i]["owner"].asString() == "Blue")
+		{
+			convertNumToString(&converter,(*root)["worlds"]["blue"].asInt(),&SQLstmt);
+		}
+		else if (objectives[i]["owner"].asString() == "Green")
+		{
+			convertNumToString(&converter,(*root)["worlds"]["green"].asInt(),&SQLstmt);
+		}
+		else
+		{
+			SQLstmt += ",0";
+		}
+		check_guildClaim(objectives[i]["claimed_by"].asString(),con);
+		SQLstmt += ",\"" + objectives[i]["claimed_by"].asString() + "\"";
+		SQLstmt += ",15"; //tick_timer
+		SQLstmt += ",\"" + objectives[i]["owner"].asString() + "\"";
+		SQLstmt += ",\"" + objectives[i]["time_claimed"].asString() + "\"";
+		SQLstmt += ",\"" + (*root)["id"].asString() + "\"";
+		SQLstmt += ",\"" + (*root)["start_time"].asString() + "\"";
+		SQLstmt += ");";
+		try
+		{
+			stmt = con->createStatement();
+			stmt->execute(SQLstmt);
+			delete stmt;
+		}
+		catch (sql::SQLException &e)
+		{
+			cout << e.what() << endl;
+		}
+		//
+	}
+}
+/* */
+void store_matchDetails(const Json::Value root, sql::Connection *con)
+{
+	stringstream converter;
+	sql::Statement *stmt;
+	string SQLstmt = "INSERT INTO match_details VALUES(";
+	SQLstmt += "\"" + root["id"].asString() + "\"";
+	SQLstmt += ",3"; //weekNumber
+	SQLstmt += ",\"" + root["start_time"].asString() + "\"";
+	SQLstmt += ",\"" + root["end_time"].asString() + "\"";
+	convertNumToString(&converter, root["worlds"]["green"].asInt(),&SQLstmt);
+	convertNumToString(&converter, root["worlds"]["blue"].asInt(),&SQLstmt);
+	convertNumToString(&converter, root["worlds"]["red"].asInt(),&SQLstmt);
+	SQLstmt += ");";
+	//
+	try
+	{
+		stmt = con->createStatement();
+		stmt->execute(SQLstmt);
+		delete stmt;
+	}
+	catch (sql::SQLException &e)
+	{
+		cout << e.what() << endl;
+	}
+}
+void get_matchDetails(string matchID, sql::Connection *con)
+{
+		Easy request;
+		stringstream matchDetails;
+		Json::Value root;
+		Json::Reader reader;
+		/* */
+		request.setOpt(cURLpp::Options::WriteStream(&matchDetails));
+		request.setOpt(Url("https://api.guildwars2.com/v2/wvw/matches/" + matchID));
+		request.perform();
+		/* */
+		bool parsingSuccessful = reader.parse(matchDetails.str(), root);
+		if (!parsingSuccessful)
+		{
+			cout << "Failed to parse configuration\n" 
+				<< reader.getFormattedErrorMessages();
+			exit(0);
+		}
+		/* */
+		if (!stored_matchDetails)
+		{
+			store_matchDetails(root, con);
+			stored_matchDetails = true;
+		}
+		for (int i = 0; i < (int)root["maps"].size(); i++)
+		{
+			store_MapData(&root, i, con);
+		}
+}
 int main (int argc, char *argv[])
 {
     try 
     {
+    	sql::mysql::MySQL_Driver *driver;
+		sql::Connection *con;
+		sql::Statement *stmt;
+		driver = sql::mysql::get_mysql_driver_instance();
+		con = driver->connect("tcp://127.0.0.1:3306", "root", "egamirrorimeht");
+		stmt = con->createStatement();
+		stmt->execute("USE Gw2Analyser");
+		delete stmt;
+		//
+    	while (1)
+		{
+			clock_t begin = clock();
+			get_matchDetails("1-4", con);
+			clock_t end = clock();
+			double elapsed_msecs = double(end - begin) / CLOCKS_PER_SEC * microSec;
+			//usleep(microSec*5 - elapsed_msecs);
+		}
+		delete con;
 		/*
-		----------------------------------------------------------
-		*/
-		/* The request to be sent */
 		Easy myRequest;
-		/* Set the options */
 		stringstream result;
 		myRequest.setOpt(cURLpp::Options::WriteStream(&result));
 		myRequest.setOpt(Url("https://api.guildwars2.com/v2/wvw/matches/1-3"));
-		/* Perform request */
 		myRequest.perform();
-		/*
-		----------------------------------------------------------
-		*/
 		Json::Value root;
 		Json::Reader reader;
 		bool parsingSuccessful = reader.parse(result.str(), root);
@@ -65,11 +223,9 @@ int main (int argc, char *argv[])
 		}
 		const Json::Value maps = root["maps"];
 		storeMapData(maps[0]);
-		/*
-		----------------------------------------------------------
-		*/
+
 		clock_t begin = clock();
-		/* */
+
 		sql::mysql::MySQL_Driver *driver;
 		sql::Connection *con;
 		sql::Statement *stmt;
@@ -82,22 +238,16 @@ int main (int argc, char *argv[])
 		res = stmt->executeQuery("SELECT * from user;");
 		while (res->next())
 		{
-			/* Access column data by alias or column name */
 			cout << res->getString(1) << endl;
 			cout << res->getString(2) << endl;
 		}
 		delete stmt;
 		delete con;
-		/* */
+		delete res;
 		clock_t end = clock();
 		double elapsed_msecs = double(end - begin) / CLOCKS_PER_SEC * microSec;
 		usleep(microSec*5 - elapsed_msecs);
-		/*
-		----------------------------------------------------------
 		*/
-	/*
-	----------------------------------------------------------
-	*/
 	}
 	catch (RuntimeError & e)
 	{
