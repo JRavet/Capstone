93a7296da6806ee155870ea8e2cb71454a8e9230
JRavet <ravetjs@plu.edu>
2016-02-29 23:50:30 -0800

    Basic functionality added. A user can single-sort by any attribute, and filter by any and all attributes (though currently, at least one attribute must be present for the query to be processed; this restriction may be lifted or altered later)

--- php/activity_analyser.php
+++ php/activity_analyser.php
@@ -19,7 +19,21 @@
 	</form>
 	<form action=\"activity_analyser.php\" method=\"GET\">
 	<table>";
-	echo "<tr><td>Match ID: </td><td><input type=\"text\" name=\"match_id\" value=\"" . $_GET["match_id"] . "\"/></td></tr> 
+	echo "<tr><td>Sort by:</td><td><select name=\"sort_by\">
+			<option value=\"activity_data.match_id\">Match ID</option>
+			<option value=\"week_num\">Week Number</option>
+			<option value=\"server_info.name\">Objective Owner</option>
+			<option value=\"owner_color\">Owner Color</option>
+			<option value=\"last_flipped\">Last Seized At</option>
+			<option value=\"claimed_at\">Claimed At</option>
+			<option value=\"tick_timer\">In-game clock time</option>
+			<option value=\"objective.name\">Objective Name</option>
+			<option value=\"objective.type\">Objective Type</option>
+			<option value=\"objective.map_type\">Map</option>
+			<option value=\"guild.guild_name\">Guild Name</option>
+			<option value=\"guild.guild_tag\">Guild Tag</option>
+		</select>
+		<tr><td>Match ID: </td><td><input type=\"text\" name=\"match_id\" value=\"" . $_GET["match_id"] . "\"/></td></tr> 
 		<tr><td>Week number: </td><td><input type=\"text\" name=\"week_num\" value=\"" . $_GET["week_num"] . "\"/></td></tr>
 		<tr><td>Objective owner: </td><td><input type=\"text\" name=\"obj_owner\" value=\"" . $_GET["obj_owner"] . "\"/></td></tr>
 		<tr><td>Owner color: </td><td><input type=\"text\" name=\"owner_color\" value=\"" . $_GET["owner_color"] . "\"/></td></tr>
@@ -57,35 +71,104 @@
 		INNER JOIN objective ON activity_data.obj_id=objective.obj_id
 		INNER JOIN server_info on activity_data.owner_server=server_info.srv_id
 		INNER JOIN (SELECT match_id, week_num FROM match_details GROUP BY match_id) as match_details on match_details.match_id=activity_data.match_id
-		WHERE activity_data.match_id = activity_data.match_id "; //spoof WHERE clause
-		
-		$activityQuery .= "and activity_data.match_id = \"1-4\" and guild_tag = \"Os\"
-		ORDER BY last_flipped;";
-		?>
-		<?php
-			echo "<table border=\"1\">";
-			echo "<th>Match ID</th><th>Week Number</th><th>Server</th><th>Color</th>
-			<th>Last Seized At</th><th>Claimed At</th><th>Tick Timer</th><th>Objective Name</th>
-			<th>Objective Type</th><th>Cardinal Direction</th><th>Map</th><th>Guild Name</th><th>Guild Tag</th>";
-			foreach ($conn->query($activityQuery) as $row)
-			{
-				echo "<tr>";
-				echo "<td>" . $row["Match ID"] . "</td>";
-				echo "<td>" . $row["Week Number"] . "</td>";
-				echo "<td>" . $row["Server"] . "</td>";
-				echo "<td>" . $row["Color"] . "</td>";
-				echo "<td>" . $row["Last Seized At"] . "</td>";
-				echo "<td>" . $row["Claimed At"] . "</td>";
-				echo "<td>" . $row["Tick Timer"] . "</td>";
-				echo "<td>" . $row["Objective Name"] . "</td>";
-				echo "<td>" . $row["Objective Type"] . "</td>";
-				echo "<td>" . $row["Cardinal Direction"] . "</td>";
-				echo "<td>" . $row["Map"] . "</td>";
-				echo "<td>" . $row["Guild Name"] . "</td>";
-				echo "<td>" . $row["Guild Tag"] . "</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
-		?>
+		WHERE activity_data.match_id = activity_data.match_id "; //spoof WHERE clause to allow additional clauses prefixed with AND
+		if (
+			$_GET["match_id"] == "" and $_GET["week_num"] == "" and $_GET["obj_owner"] == ""
+			and $_GET["owner_color"] == "" and $_GET["last_flipped_begin"] == "" and $_GET["last_flipped_end"] == ""
+			and $_GET["claimed_at_begin"] == "" and $_GET["claimed_at_end"] == "" and $_GET["tick_timer_begin"] == ""
+			and $_GET["tick_timer_end"] == "" and $_GET["obj_name"] == "" and $_G3ET["obj_type"] == ""
+			and $_GET["map_type"] == "" and $_GET["guild_name"] == "" and $_GET["guild_tag"] == ""
+		)
+		{
+			die(""); //if the user did not enter any search criteria, stop early
+		}
+		if ($_GET["match_id"] != "")
+		{
+			$activityQuery .= "and activity_data.match_id = \"" . $_GET["match_id"] . "\" ";
+		}
+		if ($_GET["week_num"] != "")
+		{
+			$activityQuery .= "and week_num = \"" . $_GET["week_num"] . "\" ";
+		}
+		if ($_GET["obj_owner"] != "")
+		{
+			$activityQuery .= "and server_info.name  = \"" . $_GET["obj_owner"] . "\" ";
+		}
+		if ($_GET["owner_color"] != "")
+		{
+			$activityQuery .= "and owner_color = \"" . $_GET["owner_color"] . "\" ";
+		}
+		if ($_GET["last_flipped_begin"] != "")
+		{
+			$activityQuery .= "and last_flipped >= \"" . $_GET["last_flipped_begin"] . "\" ";
+		}
+		if ($_GET["last_flipped_end"] != "")
+		{
+			$activityQuery .= "and last_flipped < \"" . $_GET["last_flipped_end"] . "\" ";
+		}
+		if ($_GET["claimed_at_begin"] != "")
+		{
+			$activityQuery .= "and claimed_at >= \"" . $_GET["claimed_at_begin"] . "\" ";
+		}
+		if ($_GET["claimed_at_end"] != "")
+		{
+			$activityQuery .= "and claimed_at < \"" . $_GET["claimed_at_end"] . "\" ";
+		}
+		if ($_GET["tick_timer_begin"] != "")
+		{
+			$activityQuery .= "and tick_timer >= \"" . $_GET["tick_timer_begin"] . "\" ";
+		}
+		if ($_GET["tick_timer_end"] != "")
+		{
+			$activityQuery .= "and tick_timer < \"" . $_GET["tick_timer_end"] . "\" ";
+		}
+		if ($_GET["obj_name"] != "")
+		{
+			$activityQuery .= "and objective.name = \"" . $_GET["obj_name"] . "\" ";
+		}
+		if ($_GET["obj_type"] != "")
+		{
+			$activityQuery .= "and objective.type = \"" . $_GET["obj_type"] . "\" ";
+		}
+		if ($_GET["map_type"] != "")
+		{
+			$activityQuery .= "and objective.map_type = \"" . $_GET["map_type"] . "\" ";
+		}
+		if ($_GET["guild_name"] != "")
+		{
+			$activityQuery .= "and guild_name = \"" . $_GET["guild_name"] . "\" ";
+		}
+		if ($_GET["guild_tag"] != "")
+		{
+			$activityQuery .= "and guild_tag = \"" . $_GET["guild_tag"] . "\" ";
+		}
+		$activityQuery .= "ORDER BY " . $_GET["sort_by"] . ";";
+		//
+		//
+		//
+		echo "<table border=\"1\">";
+		echo "<th>Match ID</th><th>Week Number</th><th>Server</th><th>Color</th>
+		<th>Last Seized At</th><th>Claimed At</th><th>Tick Timer</th><th>Objective Name</th>
+		<th>Objective Type</th><th>Cardinal Direction</th><th>Map</th><th>Guild Name</th><th>Guild Tag</th>";
+		foreach ($conn->query($activityQuery) as $row)
+		{
+			echo "<tr>";
+			echo "<td>" . $row["Match ID"] . "</td>";
+			echo "<td>" . $row["Week Number"] . "</td>";
+			echo "<td>" . $row["Server"] . "</td>";
+			echo "<td>" . $row["Color"] . "</td>";
+			echo "<td>" . $row["Last Seized At"] . "</td>";
+			echo "<td>" . $row["Claimed At"] . "</td>";
+			echo "<td>" . $row["Tick Timer"] . "</td>";
+			echo "<td>" . $row["Objective Name"] . "</td>";
+			echo "<td>" . $row["Objective Type"] . "</td>";
+			echo "<td>" . $row["Cardinal Direction"] . "</td>";
+			echo "<td>" . $row["Map"] . "</td>";
+			echo "<td>" . $row["Guild Name"] . "</td>";
+			echo "<td>" . $row["Guild Tag"] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	?>
 	</body>
 </html>
