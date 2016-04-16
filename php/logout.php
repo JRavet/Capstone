3f31c8ca7310a04208adaec41f70fbec5abca43f
JRavet <ravetjs@plu.edu>
2016-04-16 16:17:28 -0700

    Added an else-statement, so it always returns to the index no matter what. Added an error-code for when a session is no longer active

--- php/logout.php
+++ php/logout.php
@@ -1,4 +1,8 @@
 <?php
+	if (session_status() == PHP_SESSION_NONE)
+	{
+		header("Location: index.php?error=session_closed");
+	}
 	session_start();
 	if(session_destroy())
 	{
@@ -11,4 +15,9 @@
 			header("Location: index.php?error=". $_GET["error"]); //include whether or not there was an error when redirecting to here
 		}
 	}
+	else
+	{
+		header("Location: index.php");
+	}
 ?>
+
