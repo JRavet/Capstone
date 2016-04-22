ee1cee5539a3d6edcee2e853d4dc27d8329f82ea
JRavet <ravetjs@plu.edu>
2016-04-22 15:31:16 -0700

    Updated SQL query and output to accomodate new changes coming to the game

--- php/match_details_analyser.php
+++ php/match_details_analyser.php
@@ -26,10 +26,14 @@
 	<br/>
 	<?php
 		$matchQuery = "SELECT match_id as \"Match ID\", week_num as \"Week Number\",
-			start_time as \"Start Time\", end_time as \"End Time\", s1.name as \"Red Server\",
-			s2.name as \"Blue Server\", s3.name as \"Green Server\", grn_srv_population as \"Green Population\",
-			blu_srv_population as \"Blue Population\", red_srv_population as \"Red Population\"
+			start_time as \"Start Time\", end_time as \"End Time\", s1.name as \"Red Server\", s1_1.name as \"Red Server2\",
+			s2.name as \"Blue Server\", s2_1.name as \"Blue Server2\", s3.name as \"Green Server\", s3_1.name as \"Green Server2\",
+			grn_srv_population as \"Green Population\", blu_srv_population as \"Blue Population\", red_srv_population as \"Red Population\",
+			grn_srv2_pop as \"Green Population2\", blu_srv2_pop as \"Blue Population2\", red_srv2_pop as \"Red Population2\"
 			FROM match_details
+			LEFT JOIN server_info s1_1 on s1_1.srv_id = red_srv2
+			LEFT JOIN server_info s2_1 on s2_1.srv_id = blue_srv2
+			LEFT JOIN server_info s3_1 on s3_1.srv_id = green_srv2
 			INNER JOIN server_info s1 ON s1.srv_id = red_srv
 			INNER JOIN server_info s2 ON s2.srv_id = blue_srv
 			INNER JOIN server_info s3 ON s3.srv_id = green_srv
@@ -60,9 +64,24 @@
 			echo "<tr>";
 			echo "<td>" . $row["Match ID"] . "</td>";
 			echo "<td>" . $row["Week Number"] . "</td>";
-			echo "<td bgcolor=\"#00cc00\"><center>" . $row["Green Server"] . "<br><br>" . $row["Green Population"] . "</center></td>";
-			echo "<td bgcolor=\"#3399ff\"><center>" . $row["Blue Server"] . "<br><br>" . $row["Blue Population"] . "</center></td>";
-			echo "<td bgcolor=\"#ff5050\"><center>" . $row["Red Server"] . "<br><br>" . $row["Red Population"] . "</center></td>";
+			echo "<td bgcolor=\"#00cc00\"><center>" . $row["Green Server"] . " (" . $row["Green Population"] . ")";
+			if ($row["Green Server2"] != "")
+			{
+				echo "<br>" . $row["Green Server2"] . " (" . $row["Green Population2"] . ")";
+			}
+			echo "</center></td>";
+			echo "<td bgcolor=\"#3399ff\"><center>" . $row["Blue Server"] . " (" . $row["Blue Population"] . ")";
+			if ($row["Blue Server2"] != "")
+			{
+				echo "<br>" . $row["Blue Server2"] . " (" . $row["Blue Population2"] . ")";
+			}
+			echo "</center></td>";
+			echo "<td bgcolor=\"#ff5050\"><center>" . $row["Red Server"] . " (" . $row["Red Population"] . ")";
+			if ($row["Red Server2"] != "")
+			{
+				echo "<br>" . $row["Red Server2"] . " (" . $row["Red Population2"] . ")";
+			}
+			echo "</center></td>";
 			echo "<td>" . $row["Start Time"] . "</td>";
 			echo "<td>" . $row["End Time"] . "</td>";
 			echo "</tr>";
