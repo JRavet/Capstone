dfffc762411d17a51d948ea56032f6fbc067b849
JRavet <ravetjs@plu.edu>
2016-05-01 14:16:01 -0700

    Style change.

--- php/change_log.php
+++ php/change_log.php
@@ -3,7 +3,7 @@
 	<title> Change Log </title>
 	<style>body{background:#FFF;}</style>
 	<body>
-	<div style="height:50%;width:50%;border:1px solid #ccc;overflow:auto;">
+	<div style="height:100%;width:100%;border:1px solid #ccc;overflow:auto;">
 		<center><h3>5/1/2016</h3></center>
 		<ul>
 		<li>Change log added.</li>
